a332e0f31f4bcf4b01af02d84f812cd90f7e9f8b
Samaya Mahate <samaya.mahate@amniltech.com>
2026-04-24 10:38:52 +0545

    feat: add claim_confirmed, claim_rejected, pickup_completed notification types

--- app/Modules/Core/Enums/NotificationTypeEnum.php
+++ app/Modules/Core/Enums/NotificationTypeEnum.php
@@ -5,6 +5,9 @@ namespace App\Modules\Core\Enums;
 enum NotificationTypeEnum: string
 {
     case CLAIM_RECEIVED = 'claim_received';
+    case CLAIM_CONFIRMED = 'claim_confirmed';
+    case CLAIM_REJECTED = 'claim_rejected';
+    case PICKUP_COMPLETED = 'pickup_completed';
 
     public static function getAllValues(): array
     {
